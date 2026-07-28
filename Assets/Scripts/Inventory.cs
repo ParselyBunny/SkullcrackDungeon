59940f83a8395350edfc7057e74b47089b6f57e9
ParselyBunny <16728881+ParselyBunny@users.noreply.github.com>
2026-07-28 08:09:16 -0700

    Create a bug for Gig to solve

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,9 +1,18 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Inventory
 {
+    List<Item> _items;
+
+    // Default constructor for the Inventory
+    //   When a new Intentory object is instantiated, this
+    //   code is called to initialize the data
     public Inventory()
     {
-
+        // TODO: add default items for the inventory
+        // there should be (1) SWORD, (1) ROPE, (1) LANTERN, and (10) RATIONS
+        Item sword = new(SWORD, 1);  // Ugh, why isn't it working??? :'(
+        _items.Add("sword");
     }
 }
