3e5075fa01e40f46e648e5349e1d5555cb7e2f52
ParselyBunny <16728881+ParselyBunny@users.noreply.github.com>
2026-07-28 18:44:35 -0700

    Clean up Inventory constructor and add list allocation

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -3,25 +3,13 @@ using System.Collections.Generic;
 
 public class Inventory
 {
-    List<Item> _items;
+    List<Item> _items = new();
 
-    // Default constructor for the Inventory
-    //   When a new Intentory object is instantiated, this
-    //   code is called to initialize the data
     public Inventory()
     {
-        // TODO: add default items for the inventory
-        // there should be (1) SWORD, (1) ROPE, (1) LANTERN, and (10) RATIONS
-
-
-        Item rope = new("ROPE", 1);
-        Item lantern = new("LANTERN", 1);
-        Item ration = new("RATIONS", 10);
-
-
         _items.Add(new Item("SWORD", 1));
-        _items.Add(rope);
-        _items.Add(lantern);
-        _items.Add(ration);
+        _items.Add(new Item("ROPE", 1));
+        _items.Add(new Item("LANTERN", 1));
+        _items.Add(new Item("RATIONS", 10));
     }
 }
