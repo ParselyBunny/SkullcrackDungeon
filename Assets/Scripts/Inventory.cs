5c15b37f9f0f400e682bd8409dd8368633cc2394
ParselyBunny <16728881+ParselyBunny@users.noreply.github.com>
2026-08-06 07:13:51 -0700

    Add ability to search and modify inventory

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,4 +1,3 @@
-using UnityEngine;
 using System.Collections.Generic;
 
 public class Inventory
@@ -12,4 +11,28 @@ public class Inventory
         _items.Add(new Item("LANTERN", 1));
         _items.Add(new Item("RATIONS", 10));
     }
+
+    public int Find(string itemName)
+    {
+        for (int i = 0; i < _items.Count; i++)
+        {
+            if (_items[i] != null &&
+                _items[i].Name.ToLower() == itemName.ToLower())
+            {
+                return i;
+            }
+        }
+
+        throw new System.Exception(
+            $"Could not find item {itemName} in Inventory.");
+    }
+
+    // TODO: Come up with a different way of doing this that 
+    //   permits you to fail to find an item, without crashing the game :)
+    public int GetItemQuantity(string itemName) => 
+        _items[Find(itemName)].Quantity;
+    public void SetItemQuantity(string name, int val) =>
+        _items[Find(name)].Set(val);
+    public void AddItemQuantity(string name, int val) => 
+        _items[Find(name)].Add(val);
 }
